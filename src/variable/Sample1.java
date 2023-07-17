0212118a3727dd54bd83d8d020e5cd98257fdf80
Nexas <Nexas@NEXAS>
2023-07-17 13:50:59 +0530

    [17jully_2023]: Variable and DataTypes

---
+++ src/variable/Sample1.java
@@ -0,0 +1,28 @@
+package variable;
+
+public class Sample1
+{
+public static void main(String[] args) 
+{
+//varisble declaration
+	String studentname;
+	String studentbranch;
+	int studentrollnum;
+	float studentpercentage;
+	char studentgrade;
+	
+	//variable declaration
+	studentname="Sanjay";
+	studentbranch="IT";
+	studentrollnum=101;
+	studentpercentage=78.9f;
+	studentgrade='A';
+	
+	//variable usage
+	System.out.println("studentname: "+studentname);
+	System.out.println("studentbranch: "+studentbranch);
+	System.out.println("studentrollnum: "+studentrollnum);
+	System.out.println("studentpercentage: "+studentpercentage);
+	System.out.println("studentgrade: "+studentgrade);
+}
+}
